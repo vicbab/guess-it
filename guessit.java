6fab595230d39058ffa1f01b995605482a3201c6
Victor B <vicbab@users.noreply.github.com>
2016-09-01 16:44:01 -0400

    Add files via upload

---
+++ guessit.java
@@ -0,0 +1,48 @@
+// Code written by @vicbab
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class guessit {
+    public static int randInt(int min, int max){
+        Random rand = new Random();
+        
+        int targetNumber = rand.nextInt((max - min) + 1) + min;
+        
+        return targetNumber;
+    }
+    
+    public static void main(String[] args) {
+        int targetNumber = randInt(1, 10000);
+        Scanner guessInput = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int guess = guessInput.nextInt();
+
+        
+        while(!(guess == targetNumber)){
+            int diff = guess - 2000;
+            int gapHigh = Math.abs(diff);
+            int gapLow = guess + 2000;
+            
+            if(guess > targetNumber && gapHigh > targetNumber){
+            
+                System.out.println("Way Too High!");
+            }
+            else if(guess > targetNumber){
+                System.out.println("Too High!");
+            }
+
+            else if(guess < targetNumber && gapLow < targetNumber){
+                    
+                System.out.println("Way too low!");
+            }
+            else if(guess < targetNumber){
+                System.out.println("Too Low!");
+            }
+            System.out.println("Enter another number:");
+            guess = guessInput.nextInt();
+        }
+        System.out.println("Got it!");
+    }
+    
+}
